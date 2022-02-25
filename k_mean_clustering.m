6090b43954606cb8879038b6e052158e4b98606c
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2022-02-25 17:48:47 +0100

    Update k_mean_clustering.m

--- k_mean_clustering.m
+++ k_mean_clustering.m
@@ -8,10 +8,10 @@ JJ = zeros(m,1);
 CC = zeros(m,n_cl);
 for i=1:m
      for j=1:n_cl
-          ecc(j) = sum( (A(i,:)-MU(j,:)).^2,2 );
+          ecc(j) = sum( (A(i,:)-MU(j,:)).^2,2 );      #Distance from each centroid
      end;
-     [val q] = min(ecc);
-     CC(i,q) = 1;
+     [val q] = min(ecc);                              #Find the minimum distance and centroid index
+     CC(i,q) = 1;                                     #Assign centroid to instance
 end;
 
 for i=1:n_cl
@@ -19,10 +19,10 @@ for i=1:n_cl
      MEH = find(CC(:,i));
      for j=1:length(MEH)
           for q=1:n
-               MUg(i,q) = MUg(i,q)+A(MEH(j),q);
+               MUg(i,q) = MUg(i,q)+A(MEH(j),q);       #Sum of all data (i.e. coordinates)
           end;
      end;
-     MU(i,:) = MUg(i,:)/length(MEH);
+     MU(i,:) = MUg(i,:)/length(MEH);                  #Sum divided by the number of observation per cluster (i.e. mean)
 end;
 
 y=MU;
@@ -32,10 +32,10 @@ for i=1:n_cl
      ZZ(:,i) = ((sum(([A] - MU(i,:)).^2,2)) );
 end;
 for j=1:m
-	[val,~] = min(ZZ(j,:));
-	JJ(j) = val;
+     [val,~] = min(ZZ(j,:));                          #Minimum sum of distances
+     JJ(j) = val;
 end
 
-J = (1/m)*sum(JJ);
+J = (1/m)*sum(JJ);                                    #Cost function
 
 end;
