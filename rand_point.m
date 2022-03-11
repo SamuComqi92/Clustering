eeea47291632ef1b82a1502dcca18fb5ebbab20f
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2022-03-11 08:11:30 +0100

    Update rand_point.m

--- rand_point.m
+++ rand_point.m
@@ -1,10 +1,12 @@
-## Function to assign random point form the dataset to the initial centroids
-#############################################################################
+################################################################################
+##  The following function assigns random points, extracted form the dataset, ##
+## to initialize the centroids that will be used in the clustering algorithm. ##
+################################################################################
 
-function MU = rand_point(A, n_cl),
+function MU = rand_point(A, n_cl),        #Input parameters: dataset, number of clusters
 
-m = size(A,1);
-MU = [];
+m = size(A,1);                            #Number of rows
+MU = [];                                  #Matrix that will contain the initial centroids
 for i=1:n_cl
      MU = [MU; A(randsample(m,1),:)];
 end;
