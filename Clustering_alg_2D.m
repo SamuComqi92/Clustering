511369209f36f5aec889c33ec35cfc2e09f25763
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2022-03-11 08:32:33 +0100

    Update Clustering_alg_2D.m

--- Clustering_alg_2D.m
+++ Clustering_alg_2D.m
@@ -3,7 +3,8 @@
 ###############################################################################################################################################
 ## The code is an application of the K-mean clustering algorithm. The data file contains astrophysical data
 ## - The Eddington ratio (Log value) 
-##        ---> It represents the strength of the radiation coming from the accretion disk of gas around a black hole
+##        ---> It represents the strength of the radiation coming from the accretion disk of gas around a black hole. A small correction 
+##             (i.e. -0.8) is applied to data (it depends on the black hole rotation).
 ## - The Observed disk luminosity (Log value) 
 ##        ---> It represents the amount of radiation produced by the accretion disk
 ## - The Observed luminosity ratio (Log value) 
@@ -27,11 +28,10 @@ set (findobj (gcf, "-property", "interpreter"), "interpreter", "latex")
 
 
 
-## Randomly assign n_cl data-point from the dataset to initialize the centroids.
-## Then run K-mean algorithm. At each iteration (20 in total), the cost function 
-## (i.e., the sum of the distance of each point from the closest centroid divided 
-## by the number of data) is computed. The best centroids corresponds to those with 
-## the lowest cost function.
+## Randomly assign n_cl data-point from the dataset to initialize the centroids then run 
+## K-mean algorithm. At each iteration (20 in total), the cost function (i.e., the sum of 
+## the distance of each point from the closest centroid divided by the number of data) is 
+## computed. The best centroids are those with the lowest cost function value.
 ##############################################################################################################
 
 J_matrix = zeros(20,n_cl*size(A,2)+1);
@@ -45,22 +45,20 @@ for k=1:20,
 end;
 printf("\n\n");
 
-[val,q] = min(J_matrix(:,1));
+[val,q] = min(J_matrix(:,1));                                  
 MU = reshape(J_matrix(q,2:end),n_cl,size(A,2));
 
 MU
 
-## Bidimentional plot of the dataset
-## Different colors for different clusters
-##############################################################################################################
 
+## Bidimentional plot of the dataset. Different colors are used for different clusters
 printf("\nPlotting data, centroid positions and clusters...\n");
 ZZ = zeros(m,n_cl); 
 for i=1:n_cl
      ZZ(:,i) = ((sum(([A] - MU(i,:)).^2,2)) );
 end;
 
-[val q] = min(ZZ');
+[val q] = min(ZZ');                                          #Find the centroid set with the minimum cost function
 MEH = [];
 for i=1:n_cl
      MEH = find(q==i);
