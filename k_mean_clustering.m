f07c8064e581097b0447ae397fc4abed3a1d75ba
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2022-09-17 12:00:29 +0200

    Update k_mean_clustering.m

--- k_mean_clustering.m
+++ k_mean_clustering.m
@@ -1,8 +1,8 @@
 #################################################################################
-##   This is the K-means algorithm used for updating the centroids position    ##
-##  based on their nearest points. The algorithm returns the matrix containing ##
+## This is the K-means algorithm used for updating the centroids position      ##
+## based on their nearest points. The algorithm returns the matrix containing  ##
 ## the final centroids and the "cost function", i.e. the mean squared distance ##
-##       of each point of the dataset from the corresponding centroid.         ##
+## of each point of the dataset from the corresponding centroid.               ##
 #################################################################################
 
 function [y,J] = k_mean_clustering(A,MU,n_cl)          #Input parameters: dataset, initial centroid matrix, and number of centroids
@@ -12,15 +12,17 @@ ecc = zeros(1,n_cl);                                   #Matrix that will contain
 ZZ = zeros(m,n_cl);                                    #Matrix that will contain the distance of a point from each final centroid 
 JJ = zeros(m,1);                                       #Vector that will contain the minimum squared distance of a point from the corresponding centroid
 
+#Find the closest centroid for each point
 CC = zeros(m,n_cl);                                    #Matrix that will tell which centroid each point is closest to (1s and 0s)
 for i=1:m
      for j=1:n_cl
           ecc(j) = sum( (A(i,:)-MU(j,:)).^2,2 );       #Distance of point i from each centroid j
      end;
      [val q] = min(ecc);                               #Find the minimum distance and centroid index for point i
-     CC(i,q) = 1;                                      #Assign 1 for point i to its closest centroid q
+     CC(i,q) = 1;                                      #Assign 1 for point i to its closest centroid q (0 elsewhere)
 end;
 
+#Mean coordinate values for each space axis
 for i=1:n_cl
      MUg = zeros(n_cl,size(A,2));
      MEH = find(CC(:,i));
@@ -29,10 +31,11 @@ for i=1:n_cl
                MUg(i,q) = MUg(i,q)+A(MEH(j),q);        #Sum of all data (i.e. sum of coordinates)
           end;
      end;
-     MU(i,:) = MUg(i,:)/length(MEH);                   #Sum divided by the number of observation per cluster (i.e. mean value for each coordinate)
+     MU(i,:) = MUg(i,:)/length(MEH);                   
 end;
 
-y = MU;                                                #Matrix containing the final centroid coordinates
+#Matrix containing the final centroid coordinates
+y = MU;                                                
 
 
 ## The following function computes the minimum "cost function" (i.e. minimum mean squared distance) and chose the best centroids set
